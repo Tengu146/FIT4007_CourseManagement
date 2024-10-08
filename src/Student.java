c4f3f6a0f1b3e7c51a0005fd09c214248ff11154
vphuc10 <vanphuc101005@gmail.com>
2024-10-08 22:53:33 +0700

    phuc update

--- src/Student.java
+++ src/Student.java
@@ -1,24 +1,41 @@
 public class Student {
     private String studentId;
     private String name;
+    private String birthDate;
+    private String phoneNumber;
+    private String email;
 
-    public Student(String studentId, String name) {
+    public Student(String studentId, String name, String birthDate, String phoneNumber, String email) {
         this.studentId = studentId;
         this.name = name;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
-
     public String getStudentId() {
         return studentId;
     }
-
     public String getName() {
         return name;
     }
+    public String getBirthDate() {
+        return birthDate;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
                 "studentId='" + studentId + '\'' +
                 ", name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
